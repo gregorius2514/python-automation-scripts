9927cd6fcc080d481484ea39f30872304200cc7b
grzesiek <grzesiek_kielce@o2.pl>
2020-08-16 13:10:20 +0200

    Find duplicates of files

--- find-file-duplicats/ConfigLoader.py
+++ find-file-duplicats/ConfigLoader.py
@@ -1,20 +1,14 @@
-import yaml
+#import yaml
 
 class ConfigLoader():
-    def __init__(self, configFilePath = 'config.yml'):
-        with open(configFilePath, 'r') as configFile:
-            self.logFilePath ="/tmp/dummy.log"
-            self.rootFilePath = "/tmp"
+    def __init__(self):
+        # with open(configFilePath, 'r') as configFile:
+        self.logFilePath ="/tmp/dummy.log"
+        self.rootFilePath = "/tmp"
 
-            try:
-                configuration = yaml.load(configFile)
-                self.logFilePath = configuration.logger.logFilePath
-                self.rootFilePath = configuration.finder.rootFilePath
-            except yaml.YAMLError as parseError:
-                print('Error when try to parse config file: [{}], error: '.format(configFile, parseError))
-
-    def logFilePath(self):
-        return self.logFilePath
-
-    def rootFilePath(self):
-        return self.rootFilePath
+            # try:
+            #     configuration = yaml.load(configFile)
+            #     self.logFilePath = configuration.logger.logFilePath
+            #     self.rootFilePath = configuration.finder.rootFilePath
+            # except yaml.YAMLError as parseError:
+            #     print("Error when try to parse config file: [{}], error: ".format(configFile, parseError))
--- find-file-duplicats/Main.py
+++ find-file-duplicats/Main.py
@@ -1,6 +1,11 @@
 import os
-import ConfigLoader
 import hashlib
+
+class ConfigLoader():
+    def __init__(self):
+        # with open(configFilePath, 'r') as configFile:
+        self.logFilePath ="/tmp/dummy.log"
+        self.rootFilePath = "/tmp"
 
 class Logger():
     def __init__(self):
@@ -16,16 +21,24 @@ class DuplicatsFinder():
         duplicatedFilePaths = {}
         for root, _, files in os.walk(self.rootDirPath):
             for filename in files:
-                fileHash = self.hashFile(filename)
-                if fileHash in duplicatedFilePaths:
-                    foundSimilarDuplicatedFilePaths = duplicatedFilePaths[fileHash]
-                    foundSimilarDuplicatedFilePaths.append("{}/{}".format(root,filename))
-                else:
-                    duplicatedFilePaths[fileHash] = ["{}/{}".format(root, filename)]
+                try:
+                    fileHash = self.hashFile("{}/{}".format(root,filename))
+                    if fileHash in duplicatedFilePaths:
+                        foundSimilarDuplicatedFilePaths = duplicatedFilePaths[fileHash]
+                        foundSimilarDuplicatedFilePaths.append("{}/{}".format(root,filename))
+                    else:
+                        duplicatedFilePaths[fileHash] = ["{}/{}".format(root, filename)]
+                except FileNotFoundError as error:
+                    print("Error: {}, filename: {}".format(error.strerror, error.filename))
+                except PermissionError as error:
+                    print("Error: {}, filename: {}".format(error.strerror, error.filename))
+                except:
+                    pass
 
         for file in duplicatedFilePaths.values():
             if (len(file) > 1):
                 print(duplicatedFilePaths)
+                # write info to log file
 
     def hashFile(self, filepath):
         READ_FILE_CHUNK = 2048
@@ -39,5 +52,7 @@ class DuplicatsFinder():
 
 if __name__ == "__main__":
     configLoader = ConfigLoader()
-    d = DuplicatsFinder(configLoader.rootFilePath())
+    d = DuplicatsFinder(configLoader.rootFilePath)
     d.find()
+
+
